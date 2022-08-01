1a55208f5fefee085cc349747283a159537dd261
thomasandr <andrew.ja.thomas@gmail.com>
2022-08-01 09:12:26 -0600

    fix comments

--- src/sql/origin.sql
+++ src/sql/origin.sql
@@ -1,16 +1,16 @@
 select
-	# add basic lead info
+	-- add basic lead info
 	 o.[record] as [person_id]
 	,(select [value] from [lookup.prompt] where [id] = (select top 1 [prompt] from [field] where [field] = 'college_of_interest' and [record] = o.[record] order by [timestamp] asc)) as [college_of_interest]
 	
-	# add conversion/origin info
+	-- add conversion/origin info
 	,o.[date] as [origin_date]
 	,a.[created] as [conversion_date]
 	,case when a.[id] is not null then 1 else 0 end as [conversion_ind]
 	,lot.[summary] as [origin_summary]
 	,o.[memo] as [origin_memo]
 	
-	# add aggregated stats on mailing, activity, pings, etc.
+	-- add aggregated stats on mailing, activity, pings, etc.
 	,(select sum(case when [status] in ('sent', 'open', 'complaint', 'delivered', 'click', 'optout') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [sent]
 	,(select sum(case when [status] in ('open', 'complaint', 'click', 'optout') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [sent]
 	,(select sum(case when [status] in ('sent', 'open', 'complaint', 'delivered', 'click', 'optout') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [open]
@@ -22,14 +22,14 @@ select
 	,(select count(*) from [activity] where [code] = 'phone call' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [phone_call_count] 
 	,(select count(*) from [activity] where [code] = 'walkin' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [walkin_count] 
 
-# start with origin data
+-- start with origin data
 from [origin] o
 left join [lookup.origin.type] lot on (o.[type] = lot.[id])
-# join to application table (only use first application)
+-- join to application table (only use first application)
 left join (select [id], [person], [created], row_number() over (partition by [person] order by [created] asc) as [rank_rev] from [application] where [person] is not null) a on (a.[person] = o.[record] and a.[rank_rev]=1)
 where 1=1 
 and o.[first] = 1
-# remove apps without a lead
+-- remove apps without a lead
 and lot.[summary] != 'Application'
-# remove test records
+-- remove test records
 and (o.[record] NOT IN (select [record] from [tag] where ([tag] in ('test'))))
