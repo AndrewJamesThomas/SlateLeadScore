0afcc361d60e142fdbc0edcd8aca3dc55ee7eb4f
thomasandr <andrew.ja.thomas@gmail.com>
2022-08-02 09:31:52 -0600

    Fix data leak, simplify message fields, remove origin_memo

--- src/sql/origin.sql
+++ src/sql/origin.sql
@@ -1,4 +1,4 @@
-select
+select top 100
 	-- add basic lead info
 	 o.[record] as [person_id]
 	,(select [value] from [lookup.prompt] where [id] = (select top 1 [prompt] from [field] where [field] = 'college_of_interest' and [record] = o.[record] order by [timestamp] asc)) as [college_of_interest]
@@ -8,18 +8,63 @@ select
 	,a.[created] as [conversion_date]
 	,case when a.[id] is not null then 1 else 0 end as [conversion_ind]
 	,lot.[summary] as [origin_summary]
-	,o.[memo] as [origin_memo]
 
 	-- add aggregated stats on mailing, activity, pings, etc.
-	,(select sum(case when [status] in ('sent', 'open', 'complaint', 'delivered', 'click', 'optout') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [sent]
-	,(select sum(case when [status] in ('open', 'complaint', 'click', 'optout') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [open]
-	,(select sum(case when [status] in ('click') then 1 else null end) from [message] where [mailing] is not null and [person] = o.[record]  and ([delivered] <= a.[created] or a.[created] is null)) as [click]
-	,(select count(distinct [url]) from [ping] where [record] = o.[record] and ([timestamp] <= a.[created] or a.[created] is null)) as [ping_count]
-	,(select count(*) from [activity] where [code] = 'appointment' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [apt_count]
-	,(select count(*) from [activity] where [code] = 'chat' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [chat_count]
-	,(select count(*) from [activity] where [code] = 'email' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [email_count]
-	,(select count(*) from [activity] where [code] = 'phone call' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [phone_call_count]
-	,(select count(*) from [activity] where [code] = 'walkin' and [record] = o.[record] and ([date] <= a.[created] or a.[created] is null)) as [walkin_count]
+	,(select count(*) from [message] 
+			where [status] in ('sent', 'open', 'complaint', 'delivered', 'click', 'optout')
+			and [mailing] is not null
+			and [person] = o.[record] 
+			and ([delivered] <= a.[created] or a.[created] is null)
+			and ([delivered] <= dateadd(year, 1, o.[date]))) as [sent]
+
+	,(select count(*) from [message] 
+			where [status] in ('open', 'complaint', 'click', 'optout')
+			and [mailing] is not null
+			and [person] = o.[record] 
+			and ([delivered] <= a.[created] or a.[created] is null)
+			and ([delivered] <= dateadd(year, 1, o.[date]))) as [open]
+
+	,(select count(*) from [message] 
+			where [status] in ('click')
+			and [mailing] is not null
+			and [person] = o.[record] 
+			and ([delivered] <= a.[created] or a.[created] is null)
+			and ([delivered] <= dateadd(year, 1, o.[date]))) as [click]
+
+	,(select count(distinct [url]) from [ping] 
+			where [record] = o.[record] 
+			and ([timestamp] <= a.[created] or a.[created] is null)
+			and ([timestamp] <= dateadd(year, 1, o.[date]))) as [ping_count]
+
+	,(select count(*) from [activity] 
+			where [code] = 'appointment' 
+			and [record] = o.[record] 
+			and ([date] <= a.[created] or a.[created] is null)
+			and ([date] <= dateadd(year, 1, o.[date]))) as [apt_count]
+
+	,(select count(*) from [activity] 
+			where [code] = 'chat' 
+			and [record] = o.[record] 
+			and ([date] <= a.[created] or a.[created] is null)
+			and ([date] <= dateadd(year, 1, o.[date]))) as [chat_count]
+
+	,(select count(*) from [activity] 
+			where [code] = 'email' 
+			and [record] = o.[record] 
+			and ([date] <= a.[created] or a.[created] is null)
+			and ([date] <= dateadd(year, 1, o.[date]))) as [email_count]
+
+	,(select count(*) from [activity] 
+			where [code] = 'phone call' 
+			and [record] = o.[record] 
+			and ([date] <= a.[created] or a.[created] is null)
+			and ([date] <= dateadd(year, 1, o.[date]))) as [phone_call_count]
+
+	,(select count(*) from [activity] 
+			where [code] = 'walkin' 
+			and [record] = o.[record] 
+			and ([date] <= a.[created] or a.[created] is null)
+			and ([date] <= dateadd(year, 1, o.[date]))) as [walkin_count]
 
 -- start with origin data
 from [origin] o
