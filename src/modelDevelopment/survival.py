7ed849d0b61889ae4503178cefc55968c61364c0
thomasandr <andrew.ja.thomas@gmail.com>
2022-08-04 22:00:08 -0600

    finalize survival models

--- src/modelDevelopment/survival.py
+++ src/modelDevelopment/survival.py
@@ -1,47 +1,77 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from scipy.stats import uniform
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sksurv.nonparametric import kaplan_meier_estimator
 from sksurv.linear_model import CoxPHSurvivalAnalysis
-from sklearn.metrics import accuracy_score, roc_auc_score, log_loss, balanced_accuracy_score
-from sksurv.metrics import cumulative_dynamic_auc
+from sklearn.model_selection import RandomizedSearchCV
+from lifelines import CoxPHFitter
+from sklearn.metrics import log_loss, roc_auc_score, accuracy_score, f1_score
+import tqdm
 
-# setup data
-df = pd.read_csv("data/proofOfConcept.csv")
-# check missing values
-df.isna().mean() # all good
+# import data
+X_train = pd.read_csv("data/clean/X_train.csv")
+y_train = pd.read_csv("data/clean/y_train.csv")
 
-# fix data types
-df[['origin_date', "conversion_date"]] = df[['origin_date', "conversion_date"]].apply(pd.to_datetime)
+X_test = pd.read_csv("data/clean/X_test.csv")
+y_test = pd.read_csv("data/clean/y_test.csv")
 
-# calculate days between origin date and conversion date
-df["survival_time"] = (df["conversion_date"] - df["origin_date"]).dt.days
-# if a record has no conversion, imput with 1 year
-df["survival_time"] = df["survival_time"].fillna(365)
-# fill outlies with 365
-df.loc[df["survival_time"]>365, "survival_time"] = 365
-df.loc[df["survival_time"]==365, 'conversion'] = 0
+X_train.loc[X_train["days_to_convert"] > 365, "days_to_convert"] = 365
+X_train.loc[X_train["days_to_convert"] < 0, "days_to_convert"] = 0
 
-# switch the conversions
-df = df[df["survival_time"]>=0]
+y_train[X_train["days_to_convert"]>365] = 0
+y_test[X_test["days_to_convert"]>365] = 0
 
-# split into training/test x/y datasets
-xtrain, xtest, ytrain, ytest = train_test_split(df["survival_time"], df[["conversion"]],
-                                                train_size=0.8, random_state=43)
+# combine training data
+train_combined = X_train.join(y_train)
+
+# Unregularized survival model
+
+cph = CoxPHFitter()
+cph.fit(train_combined[['email_count', 'phone_call_count', 'walkin_count', 'CTOR',
+                        'OR', 'days_to_convert', 'conversion_ind', 'ping_count_cut_med', 'ping_count_cut_high',
+                        'sent_cut_med', 'sent_cut_high', 'sent_cut_very_high', 'open_cut_med', 'open_cut_high',
+                        'origin_summary_Event Attendance', 'origin_summary_Event Registration',
+                        'origin_summary_Inquiry Form', 'origin_summary_Source', 'origin_summary_Vendor Lead'
+                        ]],
+        duration_col="days_to_convert", event_col="conversion_ind")
+
+# convert cumulative hazard function to probability to convert
+predictions = 1-cph.predict_cumulative_hazard(X_test).T
+predictions = np.subtract(predictions.T, predictions[365])
+
+# calculate auc over time
+auc_over_time = pd.DataFrame(data={"model": [],
+                                   "days": [],
+                                   "AUC": []})
+
+for i in tqdm.tqdm(predictions.index[1:-1]):
+    y_true = y_test[X_test["days_to_convert"]>i]
+    y_pred = predictions.T[X_test["days_to_convert"]>i][i]
+
+    value = log_loss(y_true, y_pred)
+    auc_over_time.loc[len(auc_over_time.index)]=["Survival", i, value]
 
 
-time, survival_prob = kaplan_meier_estimator(ytrain==1, xtrain)
+# this model performed less well than the static model, but this has no regularization
+# TODO: Regularized survival model
+cph2 = CoxPHFitter(penalizer=0.8)
+cph2.fit(train_combined[['email_count', 'phone_call_count', 'walkin_count', 'CTOR',
+                        'OR', 'days_to_convert', 'conversion_ind', 'ping_count_cut_med', 'ping_count_cut_high',
+                        'sent_cut_med', 'sent_cut_high', 'sent_cut_very_high', 'open_cut_med', 'open_cut_high',
+                        'origin_summary_Event Attendance', 'origin_summary_Event Registration',
+                        'origin_summary_Inquiry Form', 'origin_summary_Source', 'origin_summary_Vendor Lead'
+                        ]],
+        duration_col="days_to_convert", event_col="conversion_ind")
 
-plt.step(time, survival_prob - min(survival_prob))
-plt.show()
+predictions = 1-cph2.predict_cumulative_hazard(X_test).T
+predictions = np.subtract(predictions.T, predictions[365])
 
-# Evaluate
-auc_train = [(a, b) for a, b in pd.concat([ytrain==1, xtrain], axis=1).values]
-auc_test = [(a, b) for a, b in pd.concat([ytest==1, xtest], axis=1).values]
-times = np.arange(0, 350, 1)
+for i in tqdm.tqdm(predictions.index[1:-1]):
+    y_true = y_test[X_test["days_to_convert"]>i]
+    y_pred = predictions.T[X_test["days_to_convert"]>i][i]
 
-auc, mean_auc = cumulative_dynamic_auc(np.array(auc_train, dtype=[('Status', '?'), ('Survival_in_days', '<f8')]),
-                                       np.array(auc_test, dtype=[('Status', '?'), ('Survival_in_days', '<f8')]),
-                                       survival_prob, times)
+    value = log_loss(y_true, y_pred)
+    auc_over_time.loc[len(auc_over_time.index)]=["Survival2", i, value]
+
+auc_over_time.query("model=='Survival2'").plot(x="days", y="AUC")
