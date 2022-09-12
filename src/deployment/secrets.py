ae7e09fedbf13f24ade84b1884510c60c088ded1
thomasandr <andrew.ja.thomas@gmail.com>
2022-09-12 15:20:39 -0600

    Create secrets.py

---
+++ src/deployment/secrets.py
@@ -0,0 +1,10 @@
+# Please upload these variables accordingly and do not save them to github.
+
+# from Slate custom SQL Query
+H = ""
+
+# Query ID for custom SQL Query
+ID = ""
+
+# SFTP Password
+PASSWORD = ""
