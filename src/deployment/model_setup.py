12e4a15809a2e745c8ef6df8bf7e1af765dd5be3
thomasandr <andrew.ja.thomas@gmail.com>
2022-09-12 14:56:58 -0600

    pep8 compliance!

--- src/deployment/model_setup.py
+++ src/deployment/model_setup.py
@@ -2,7 +2,7 @@ from src.deployment.ModelDeployment import LeadScoreModel
 from src.deployment.secrets import H, ID
 
 if __name__=="__main__":
-    #import data
+    # import data
     model = LeadScoreModel(h=H, id=ID, years_min=5, years_max=4)
     model.process_data(["person_id", "college_of_interest"])
     model.train_model()
